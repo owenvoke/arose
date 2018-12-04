13d1f810ef4eb5206ff7825060ac986b4d1f459a
pxgamer <owzie123@gmail.com>
2018-12-04 22:46:25 +0000

    Fix Psalm issues

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -13,14 +13,14 @@ class Authenticate extends Middleware
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request $request
-     * @return string|null
+     * @return string
      */
-    protected function redirectTo($request): ?string
+    protected function redirectTo($request): string
     {
         if (!$request->expectsJson()) {
             return route('login');
         }
 
-        return null;
+        return '';
     }
 }
